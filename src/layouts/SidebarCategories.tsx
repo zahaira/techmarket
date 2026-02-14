123479b506d965859fb6e91b1cc29762fa006246
zahairaJaada <zahairajaada@gmail.com>
2026-02-14 20:19:59 +0100

    fix sidebar overflow x

--- src/layouts/SidebarCategories.tsx
+++ src/layouts/SidebarCategories.tsx
@@ -50,6 +50,7 @@ const SidebarCategories = ({ isOpen, setIsOpen }: SidebarProps) => {
           fixed top-0 h-full bg-white shadow-lg border-r border-gray-200 z-50 
           flex flex-col items-start py-4
           transition-transform duration-300 ease-in-out
+          overflow-x-hidden
           ${locale === "ar" ? "right-0" : "left-0"}
           ${isOpen ? "translate-x-0" : locale === "ar" ? "translate-x-full sm:translate-x-0" : "-translate-x-full sm:translate-x-0"}
         `}
